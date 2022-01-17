36876b0587cfb253ce3e69c1ef420ef8a24ae858
hoangducviet1993 <ducviet12vl@gmail.com>
2022-01-17 15:38:06 +0700

    bt5

--- bai4+5/angular-hackernews-app/src/app/progress-bar/progress-bar.component.ts
+++ bai4+5/angular-hackernews-app/src/app/progress-bar/progress-bar.component.ts
@@ -8,21 +8,18 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ProgressBarComponent implements OnInit {
   @Input() backgroundColor = '#D9D9D9';
   @Input() progressColor = '#4CAF50';
-  @Input() progress = 0 ;
+  @Input() progress = 0;
 
-  constructor() {
-  }
+  constructor() { }
 
   ngOnInit(): void {
     this.loading();
   }
-
   loading() {
     let interval = setInterval(() => {
       this.progress = +this.progress + 5;
       if (this.progress == 100) {
         clearInterval(interval);
       }
-    }, 1000);
-  }
+    }, 1000);}
 }
