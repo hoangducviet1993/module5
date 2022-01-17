6aea6063f55d4a27a542d7b303baf93ddb227f2e
hoangducviet1993 <ducviet12vl@gmail.com>
2022-01-17 15:10:29 +0700

    bt

---
+++ angular-change-font-size/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+import { PetComponent } from './pet/pet.component';
+import { BoostrapComponent } from './boostrap/boostrap.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FontSizeEditorComponent,
+    PetComponent,
+    BoostrapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
