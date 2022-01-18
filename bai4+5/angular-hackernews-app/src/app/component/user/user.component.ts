38d8a05c1adda1e847173138ba58113d79ec1b60
hoangducviet1993 <ducviet12vl@gmail.com>
2022-01-18 11:03:02 +0700

    bai tap tao form

--- bai4+5/angular-hackernews-app/src/app/component/user/user.component.ts
+++ bai4+5/angular-hackernews-app/src/app/component/user/user.component.ts
@@ -11,17 +11,17 @@ export class UserComponent implements OnInit {
   lisUser: User[] = [];
   formUser = new FormGroup({
     email: new FormControl('',[Validators.required,Validators.email]),
-    password: new FormControl(''),
-    country: new FormControl(''),
-    age: new FormControl(''),
-    gender: new FormControl(''),
-    phone: new FormControl(''),
+    password: new FormControl('',Validators.required),
+    country: new FormControl('',Validators.required),
+    age: new FormControl('',[Validators.required,Validators.min(16)]),
+    gender: new FormControl('',Validators.required),
+    phone: new FormControl('',[Validators.required,Validators.pattern('(84|0[3|5|7|8|9])+([0-9]{8})\\b')]),
 
   })
 
-  get user() {
-    return this.formUser.get('email')
-  }
+  // get user() {
+  //   return this.formUser.get('email')
+  // }
 
   constructor() {
   }
@@ -30,6 +30,7 @@ export class UserComponent implements OnInit {
   }
 
   addNewUser() {
+    alert("Đăng ký thành công!!!")
     this.lisUser.push(this.formUser.value)
   }
 }
